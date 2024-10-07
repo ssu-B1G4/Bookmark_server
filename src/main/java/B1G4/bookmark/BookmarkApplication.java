1583632784c9efccd55dda69d94a9647466f9719
sangeun <olsang1203@gmail.com>
2024-10-07 17:11:14 +0900

    first commit

---
+++ src/main/java/B1G4/bookmark/BookmarkApplication.java
@@ -0,0 +1,13 @@
+package B1G4.bookmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkApplication.class, args);
+	}
+
+}
