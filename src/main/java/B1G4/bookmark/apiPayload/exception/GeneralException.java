eda794962c2dd5895da59356f7bc9dd939b764c1
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-10-09 02:43:31 +0900

    [add/#1] Exception 추가

---
+++ src/main/java/B1G4/bookmark/apiPayload/exception/GeneralException.java
@@ -0,0 +1,21 @@
+package B1G4.bookmark.apiPayload.exception;
+
+import B1G4.bookmark.apiPayload.code.BaseErrorCode;
+import B1G4.bookmark.apiPayload.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus() {
+        return this.code.getReasonHttpStatus();
+    }
+}
