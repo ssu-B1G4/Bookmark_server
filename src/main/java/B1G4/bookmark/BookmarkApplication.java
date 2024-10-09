c202e26898889d71740ebc40f567d5c97b9b5fe8
sangeun <olsang1203@gmail.com>
2024-10-09 17:42:53 +0900

    [add/#2] BaseEntity 추가

--- src/main/java/B1G4/bookmark/BookmarkApplication.java
+++ src/main/java/B1G4/bookmark/BookmarkApplication.java
@@ -2,8 +2,10 @@ package B1G4.bookmark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookmarkApplication {
 
 	public static void main(String[] args) {
