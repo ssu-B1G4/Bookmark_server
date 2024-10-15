51ea0d9e6b2799699010ae6c0efa35ddec418bfd
sangeun <olsang1203@gmail.com>
2024-10-16 00:38:22 +0900

    [fix] ReportPlaceRepository 오류 해결

--- src/main/java/B1G4/bookmark/repository/ReportPlaceRepository.java
+++ src/main/java/B1G4/bookmark/repository/ReportPlaceRepository.java
@@ -1,6 +1,7 @@
 package B1G4.bookmark.repository;
 
+import B1G4.bookmark.domain.ReportPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReportPlaceRepository extends JpaRepository<ReportPlaceRepository, Long> {
+public interface ReportPlaceRepository extends JpaRepository<ReportPlace, Long> {
 }
