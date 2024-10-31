4cb84a04f70eba5eb6f40eb31bd65ad5b44f808b
sangeun <olsang1203@gmail.com>
2024-11-01 00:15:11 +0900

    [feat/#18] 하버사인 공식을 이용한 반경 내 공간 조회 및 거리순 정렬 구현

--- src/main/java/B1G4/bookmark/repository/PlaceRepository.java
+++ src/main/java/B1G4/bookmark/repository/PlaceRepository.java
@@ -1,7 +1,35 @@
 package B1G4.bookmark.repository;
 
 import B1G4.bookmark.domain.Place;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
+    @Query(value = """
+   SELECT p.*, (6371 * acos(
+       cos(radians(:latitude)) * 
+       cos(radians(p.latitude)) * 
+       cos(radians(p.longitude) - radians(:longitude)) + 
+       sin(radians(:latitude)) * 
+       sin(radians(p.latitude))
+   )) as distance 
+   FROM Place p
+   WHERE p.latitude BETWEEN :minLat AND :maxLat 
+   AND p.longitude BETWEEN :minLng AND :maxLng
+   HAVING distance <= :radius
+   ORDER BY distance
+   """, nativeQuery = true)
+    Page<Place> findByBoundingBox(
+            @Param("minLat") double minLat,
+            @Param("maxLat") double maxLat,
+            @Param("minLng") double minLng,
+            @Param("maxLng") double maxLng,
+            @Param("latitude") double latitude,
+            @Param("longitude") double longitude,
+            @Param("radius") double radius,
+            PageRequest pageRequest
+    );
 }
