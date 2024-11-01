16db6d3c26d4cf181355584d41c415046a650cb1
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-11-01 20:09:31 +0900

    [feat/#8] redis 연결 설정

--- src/main/java/B1G4/bookmark/BookmarkApplication.java
+++ src/main/java/B1G4/bookmark/BookmarkApplication.java
@@ -3,9 +3,11 @@ package B1G4.bookmark;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableRedisRepositories
 public class BookmarkApplication {
 
 	public static void main(String[] args) {
