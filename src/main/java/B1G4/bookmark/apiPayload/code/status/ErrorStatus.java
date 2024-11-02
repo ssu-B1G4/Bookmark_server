872a9f704d9ab942d487e1eb5496517c501f58d8
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-11-03 04:27:07 +0900

    [refactor/#8] 카카오 errorcode 추가

--- src/main/java/B1G4/bookmark/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/B1G4/bookmark/apiPayload/code/status/ErrorStatus.java
@@ -31,6 +31,7 @@ public enum ErrorStatus implements BaseErrorCode {
     NOT_EQUAL_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_004", "리프레시 토큰이 다릅니다."),
     NOT_CONTAIN_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_005", "해당하는 토큰이 저장되어있지 않습니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "AUTH_006", "비밀번호가 일치하지 않습니다."),
+    INVALID_REQUEST_INFO(HttpStatus.UNAUTHORIZED, "AUTH_007", "카카오 정보 불러오기에 실패하였습니다."),
 
     // User 관련
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_001", "존재하지 않는 사용자입니다."),
