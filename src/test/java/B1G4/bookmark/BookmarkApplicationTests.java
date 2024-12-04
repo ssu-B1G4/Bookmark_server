2f9985703e345ca6a47445fa180aa5a2b0b6aafe
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-12-04 20:32:18 +0900

    Update BookmarkApplicationTests.java

--- src/test/java/B1G4/bookmark/BookmarkApplicationTests.java
+++ src/test/java/B1G4/bookmark/BookmarkApplicationTests.java
@@ -3,7 +3,7 @@ package B1G4.bookmark;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BookmarkApplicationTests.class)
 class BookmarkApplicationTests {
 
 	@Test
