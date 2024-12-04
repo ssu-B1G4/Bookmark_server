77e5fd956a3d00e9e04375dd2a556dff56bb4811
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-12-05 01:01:26 +0900

    [Update] deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ APP_DIR="/home/ubuntu/cicd"
 LOG_FILE="$APP_DIR/nohup.out"
 
 # 최신 JAR 파일 가져오기
-JAR_FILE=$(ls -t $APP_DIR/*.jar | head -n 1) # 최신 파일만 선택
+JAR_FILE=$(ls -t $APP_DIR/libs/*.jar | head -n 1) # 최신 파일만 선택
 echo "Found JAR file: $JAR_FILE"
 
 # 실행 중인 애플리케이션 종료
