9970b8aa25a0125cbb15a5d434f5b0680488a550
bingseok <qqq7384@naver.com>
2024-12-06 16:09:50 +0900

    [fix] 이미지 s3에 업로드 변경

--- src/main/java/B1G4/bookmark/service/ReportPlaceService/ReportPlaceServiceImpl.java
+++ src/main/java/B1G4/bookmark/service/ReportPlaceService/ReportPlaceServiceImpl.java
@@ -4,6 +4,7 @@ import B1G4.bookmark.converter.ReportPlaceConverter;
 import B1G4.bookmark.domain.Member;
 import B1G4.bookmark.domain.ReportPlace;
 import B1G4.bookmark.repository.ReportPlaceRepository;
+import B1G4.bookmark.service.S3Service.S3ImageServiceImpl;
 import B1G4.bookmark.web.dto.ReportPlaceDTO.ReportPlaceRequestDTO;
 import B1G4.bookmark.web.dto.ReportPlaceDTO.ReportPlaceResponseDTO;
 import lombok.RequiredArgsConstructor;
@@ -20,11 +21,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+
 @Service
 @RequiredArgsConstructor
 public class ReportPlaceServiceImpl implements ReportPlaceService {
 
     private final ReportPlaceRepository reportPlaceRepository;
+    private final S3ImageServiceImpl s3ImageService;
 
     public ReportPlaceResponseDTO createReportPlace(ReportPlaceRequestDTO requestDTO, List<MultipartFile> images, Member member) {
         List<String> imageUrls = uploadImages(images);
@@ -38,44 +41,22 @@ public class ReportPlaceServiceImpl implements ReportPlaceService {
 
     public List<String> uploadImages(List<MultipartFile> images) {
         if (images == null || images.isEmpty()) {
-            return List.of(); // 이미지가 없으면 빈 리스트 반환
+            return List.of();
         }
-
-        // 이미지 파일 저장 경로 (나중에 s3로 변경)
-        String uploadsDir = "src/main/resources/static/uploads/reportImgs/";
 
         List<String> imageUrls = new ArrayList<>();
 
         images.forEach(image -> {
             if (!image.isEmpty()) {
                 try {
-                    String dbFilePath = saveImage(image, uploadsDir);
-                    imageUrls.add(dbFilePath); // URL 추가
-                } catch (IOException e) {
+                    String s3Url = s3ImageService.upload(image);
+                    imageUrls.add(s3Url);
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         });
 
         return imageUrls;
-    }
-
-    private String saveImage(MultipartFile image, String uploadsDir) throws IOException {
-        String fileName = UUID.randomUUID().toString().replace("-", "") + "_" + image.getOriginalFilename();
-        Path uploadsPath = Paths.get(uploadsDir).toAbsolutePath();
-
-        if (!Files.exists(uploadsPath)) {
-            Files.createDirectories(uploadsPath);
-        }
-
-        Path filePath = uploadsPath.resolve(fileName);
-        String dbFilePath = "/uploads/reportImgs/" + fileName;
-
-        File file = filePath.toFile();
-        try (FileOutputStream fos = new FileOutputStream(file)) {
-            fos.write(image.getBytes());
-        }
-
-        return dbFilePath;
     }
 }
